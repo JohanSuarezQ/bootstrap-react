2d9ee7a042e00a0dbd1dac5e897456ffa5ceed1e
Johán Suárez <32972612+JohanSuarezQ@users.noreply.github.com>
2020-09-13 19:50:42 -0500

    Updates

---
+++ precache-manifest.777f15144dc8cd4432c10d010079d52e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb46567b9ea79bcf550fd76944062d05",
+    "url": "/bootstrap-react%20/index.html"
+  },
+  {
+    "revision": "13b524aa6f1542c35194",
+    "url": "/bootstrap-react%20/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bef0a81ffb9413332e2e",
+    "url": "/bootstrap-react%20/static/css/main.9496828b.chunk.css"
+  },
+  {
+    "revision": "13b524aa6f1542c35194",
+    "url": "/bootstrap-react%20/static/js/2.ed89b980.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bootstrap-react%20/static/js/2.ed89b980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef0a81ffb9413332e2e",
+    "url": "/bootstrap-react%20/static/js/main.957f941e.chunk.js"
+  },
+  {
+    "revision": "9875b0017e43f888dcad",
+    "url": "/bootstrap-react%20/static/js/runtime-main.c8233a4a.js"
+  }
+]);
