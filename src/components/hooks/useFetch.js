ba32198f80938235857599a5e114498f39015c2d
Johán Suárez <32972612+JohanSuarezQ@users.noreply.github.com>
2020-09-13 20:19:21 -0500

    changes in fecth

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -12,9 +12,7 @@ export const useFetch = ( ) => {
             const response = await fetch('https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games', {
                 method: 'POST',
                 headers: {
-                    'Access-Control-Allow-Origin': '*', 
-                    'Access-Control-Allow-Methods': 'POST',
-                    'Content-type': 'application/x-www-form-urlencoded',
+                    'Content-type': 'application/json',
                     'user-key': '3675a446bba75463e0c389758b48cfdd',
                 },
                 body: 'fields name,summary, cover.image_id;sort popularity desc;where rating >= 90;where first_release_date >1577750400 & first_release_date <1598832000;limit 10;'
